f42f661980e2acddc0134032291841eeff4f0b53
Ludmila <ludmila.drapac@cnas.gov.md>
2023-10-06 03:21:27 -0800

    completat

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int b = 10;
+
+        for (int i = 100; i < 1000; i++) {
+            if (i%5 == 0 && i%6 == 0) {
+            System.out.print(i + " ");
+
+            b++;
+            if (b % 10 == 0) {
+
+            System.out.println();
+                }
+            }
+        }
     }
 }
